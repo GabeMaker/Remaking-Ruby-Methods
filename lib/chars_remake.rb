54b66613194868b011346afdcf76d1354705cfa9
gabe <GabeMaker@users.noreply.github.com>
2015-03-21 23:09:54 +0000

    can produce 2 char arrays

--- lib/chars_remake.rb
+++ lib/chars_remake.rb
@@ -1,10 +1,11 @@
 class String
   def chars_remake
-    puts self
-    if self.length == 0
-      []
-    else
-      [self]
+    result = []
+    counter = 0
+    while result.length < self.length
+      result << self[counter]
+      counter += 1
     end
+    return result
   end
 end
--- spec/chars_remake_spec.rb
+++ spec/chars_remake_spec.rb
@@ -2,10 +2,14 @@ require 'chars_remake'
 
 describe 'String' do
   it "should recognise '' has no characters" do
-  expect("".chars_remake).to eq []
+    expect("".chars_remake).to eq []
   end
 
   it "should return one letter in array for 'a'" do
-  expect("a".chars_remake).to eq ['a']
+    expect("a".chars_remake).to eq ['a']
+  end
+
+  it "should return two letters in array for 'ab'" do
+    expect("ab".chars_remake).to eq ['a','b']
   end
 end
